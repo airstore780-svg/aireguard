94d03de1e1c5cda4deeada4782cdf7f2e8be3193
airstore780-svg <airstore780@gmail.com>
2026-02-05 21:56:39 +0200

    Update listener.js

--- listener.js
+++ listener.js
@@ -5,6 +5,7 @@ es.addEventListener("ping", () => {
   console.log("Ping received (SSE event)");
 
   // Text-to-speech
-  const utterance = new SpeechSynthesisUtterance("Ping received");
+  const utterance = new SpeechSynthesisUtterance("security alert. Your loved one is in danger. Check the live location they sent you on whatsapp, and call them. Call the police if they don't answer.");
   speechSynthesis.speak(utterance);
 });
+
